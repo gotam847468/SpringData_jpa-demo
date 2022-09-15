8a0a3a10ea6ff669baa0bd0b8c1d57fa99f6a224
gotam847468 <gotamgtm847468@gmail.com>
2022-09-15 21:02:01 +0530

    1)GraphQl controller code added to showcase book and author relation and Employee Management. 2)Removed support for Swagger Ui

--- spring-boot-data-jpa-mysql-master/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ spring-boot-data-jpa-mysql-master/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -9,20 +9,24 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 public class SpringBootDataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJpaApplication.class, args);
+    }
 
-	@Bean
+	/*@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build();
-	}
+	}*/
 }
--- spring-boot-data-jpa-mysql-master/src/main/java/com/bezkoder/spring/datajpa/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bezkoder.spring.datajpa.repository;
-
-import com.bezkoder.spring.datajpa.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
